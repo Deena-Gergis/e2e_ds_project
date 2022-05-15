ce29309cd2ad01b3a87a64c571d44882da6a0883
Deena Gergis <deena.gergis@bayer.com>
2022-05-15 16:17:33 +0200

    adding simultaion to API + minor changes

--- scripts/job_prediction_api.py
+++ scripts/job_prediction_api.py
@@ -1,23 +1,18 @@
-TRACKING_URI = "file:///home/deena_gergis/iti/iti_e2e_live/notebooks/mlruns/"
-EXPERIMENT_ID = "1"
-RUN_ID = "4c35a9c9d26a48e8bfccfae05a63d348"
-CLUSTERS_YAML_PATH = "/home/deena_gergis/iti/iti_e2e_live/data/processed/features_skills_clusters_description.yaml"
+MLFLOW_TRACKING_URI = '../models/mlruns'
+MLFLOW_RUN_ID = "a107782f5e814648af1d609b046aa929"
+CLUSTERS_YAML_PATH = "../data/processed/features_skills_clusters_description.yaml"
 
 #------------------------------------------
 
-import JobPrediction
 from JobPrediction import JobPrediction
-
-import pandas as pd
 from flask import Flask, request, jsonify
 
 #------------------------------------------
 
 # Initiate API and JobPrediction object
 app = Flask(__name__)
-job_model = JobPrediction(tracking_uri=TRACKING_URI, 
-                          experiment_id=EXPERIMENT_ID, 
-                          run_id=RUN_ID, 
+job_model = JobPrediction(mlflow_uri=MLFLOW_TRACKING_URI,
+                          run_id=MLFLOW_RUN_ID,
                           clusters_yaml_path=CLUSTERS_YAML_PATH)
 
 
@@ -25,10 +20,21 @@ job_model = JobPrediction(tracking_uri=TRACKING_URI,
 @app.route('/predict_jobs_probs', methods=['POST'])
 def predict_jobs_probs():
     available_skills = request.get_json()
-    predictions = job_model.predict_jobs_probabilities(available_skills).to_dict()    
+    predictions = job_model.predict_jobs_probabilities(available_skills).to_dict()
     return jsonify(predictions)
 
 
+# Create simulation / recommendation endpoint
+@app.route('/recommend_new_skills', methods=['POST'])
+def recommend_new_skills():
+    request_details = request.get_json()
+    available_skills = request_details['available_skills']
+    target_job = request_details['target_job']
+
+    recommended_skills = job_model.recommend_new_skills(available_skills, target_job).to_dict()
+    return jsonify(recommended_skills)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
     
